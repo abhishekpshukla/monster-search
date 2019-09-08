1421ac711bbe830c08f58fd4c19eeda2bbef46c9
Abhishek Shukla <shukla_ab@India.XoriantCorp.com>
2019-09-08 19:04:55 +0530

    Updates

---
+++ precache-manifest.d81b429fdcc96dde7fefb1cfcb617301.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa0edbe9c9d8dc2c28a0a7b1a2005b3",
+    "url": "/monster-search/index.html"
+  },
+  {
+    "revision": "9c0a03e83d0fc1bbb85c",
+    "url": "/monster-search/static/css/main.f3bd3cb0.chunk.css"
+  },
+  {
+    "revision": "89375460eeff67c8d98d",
+    "url": "/monster-search/static/js/2.2752c3e5.chunk.js"
+  },
+  {
+    "revision": "9c0a03e83d0fc1bbb85c",
+    "url": "/monster-search/static/js/main.467fd9c6.chunk.js"
+  },
+  {
+    "revision": "b5e16e0203cdda3ec974",
+    "url": "/monster-search/static/js/runtime~main.49f008f2.js"
+  }
+]);
